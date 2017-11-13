89818832fe06bcc0034059d44660656cdfc4825e
NikulinR <romannikulin1@yandex.ru>
2017-11-13 16:04:34 +0300

    version 0.00

---
+++ PaintForm/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PaintForm
+{
+    partial class FormPaint
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.gbFigure = new System.Windows.Forms.GroupBox();
+            this.rbLine = new System.Windows.Forms.RadioButton();
+            this.rbRectangle = new System.Windows.Forms.RadioButton();
+            this.rbOval = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbFigure.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(95, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(672, 495);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(69, 23);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(12, 41);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(69, 23);
+            this.btnForward.TabIndex = 2;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            // 
+            // gbFigure
+            // 
+            this.gbFigure.Controls.Add(this.rbOval);
+            this.gbFigure.Controls.Add(this.rbRectangle);
+            this.gbFigure.Controls.Add(this.rbLine);
+            this.gbFigure.Location = new System.Drawing.Point(12, 70);
+            this.gbFigure.Name = "gbFigure";
+            this.gbFigure.Size = new System.Drawing.Size(77, 99);
+            this.gbFigure.TabIndex = 3;
+            this.gbFigure.TabStop = false;
+            this.gbFigure.Text = "Figure";
+            // 
+            // rbLine
+            // 
+            this.rbLine.AutoSize = true;
+            this.rbLine.Location = new System.Drawing.Point(6, 19);
+            this.rbLine.Name = "rbLine";
+            this.rbLine.Size = new System.Drawing.Size(45, 17);
+            this.rbLine.TabIndex = 0;
+            this.rbLine.TabStop = true;
+            this.rbLine.Text = "Line";
+            this.rbLine.UseVisualStyleBackColor = true;
+            // 
+            // rbRectangle
+            // 
+            this.rbRectangle.AutoSize = true;
+            this.rbRectangle.Location = new System.Drawing.Point(6, 42);
+            this.rbRectangle.Name = "rbRectangle";
+            this.rbRectangle.Size = new System.Drawing.Size(74, 17);
+            this.rbRectangle.TabIndex = 1;
+            this.rbRectangle.TabStop = true;
+            this.rbRectangle.Text = "Rectangle";
+            this.rbRectangle.UseVisualStyleBackColor = true;
+            // 
+            // rbOval
+            // 
+            this.rbOval.AutoSize = true;
+            this.rbOval.Location = new System.Drawing.Point(6, 65);
+            this.rbOval.Name = "rbOval";
+            this.rbOval.Size = new System.Drawing.Size(47, 17);
+            this.rbOval.TabIndex = 2;
+            this.rbOval.TabStop = true;
+            this.rbOval.Text = "Oval";
+            this.rbOval.UseVisualStyleBackColor = true;
+            // 
+            // FormPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 519);
+            this.Controls.Add(this.gbFigure);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormPaint";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "FiguresPaint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbFigure.ResumeLayout(false);
+            this.gbFigure.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.GroupBox gbFigure;
+        private System.Windows.Forms.RadioButton rbOval;
+        private System.Windows.Forms.RadioButton rbRectangle;
+        private System.Windows.Forms.RadioButton rbLine;
+    }
+}
+
