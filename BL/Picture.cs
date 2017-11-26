4bd6bf153f592ec2e0f2b010a96fc61029d4b4cc
meksey <malyugin.lesha36@gmail.com>
2017-11-26 11:54:41 +0300

    добавил back и forward

--- BL/Picture.cs
+++ BL/Picture.cs
@@ -9,12 +9,13 @@ namespace BL
     public class Picture
     {
         public List<Figure> Figures = new List<Figure>();
+        public Stack<Figure> InStack = new Stack<Figure>();
         public void Delete(int x, int y)
         {
             List<Figure> Deletes = new List<Figure>();
             foreach (var item in Figures)
             {
-                if (item.Width>0 && item.Height>0)
+                if (item.Width > 0 && item.Height > 0)
                 {
                     if ((item.X < x && x < item.X + item.Width) && (item.Y < y && y < item.Y + item.Height))
                         Deletes.Add(item);
@@ -22,23 +23,36 @@ namespace BL
 
                 if (item.Width < 0 && item.Height > 0)
                 {
-                    if ((item.X + item.Width < x && x < item.X ) && (item.Y < y && y < item.Y + item.Height))
+                    if ((item.X + item.Width < x && x < item.X) && (item.Y < y && y < item.Y + item.Height))
                         Deletes.Add(item);
                 }
 
                 if (item.Width > 0 && item.Height < 0)
                 {
-                    if ((item.X < x && x < item.X + item.Width) && (item.Y + item.Height < y && y < item.Y ))
+                    if ((item.X < x && x < item.X + item.Width) && (item.Y + item.Height < y && y < item.Y))
                         Deletes.Add(item);
                 }
 
                 if (item.Width < 0 && item.Height < 0)
                 {
-                    if ((item.X + item.Width < x && x < item.X ) && (item.Y + item.Height < y && y < item.Y))
+                    if ((item.X + item.Width < x && x < item.X) && (item.Y + item.Height < y && y < item.Y))
                         Deletes.Add(item);
                 }
             }
             Figures.RemoveAll(a => Deletes.Contains(a));
+        }
+        public void Return()
+        {
+            if (Figures.Count > 0)
+            {
+                InStack.Push(Figures.ElementAt<Figure>(Figures.Count - 1));
+                Figures.RemoveAt(Figures.Count - 1);
+            }
+        }
+        public void Forward()
+        {
+            if (InStack.Count > 0)
+                Figures.Add(InStack.Pop());
         }
     }
 }
